4996eb168888aeb574e471d2639c9baf0d6b734e
JaiminVadadoriya <vadadoriyajaimin@gmail.com>
2024-12-28 20:59:12 +0530

    Refactor dropdown tree component styling and functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DropdownTreeComponent } from './dropdown-tree/dropdown-tree.component';
 import { FirstComponent } from './first/first.component';
-import { SecondComponent } from './second/second.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponent,
-    SecondComponent,
     DropdownTreeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    MatIconModule
   ],
   providers: [
     provideClientHydration()
